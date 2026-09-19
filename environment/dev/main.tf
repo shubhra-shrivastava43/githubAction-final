90f231550d567b7e1f294c232d4510eb25af249a
Shubhra <shubhranisha8@gmail.com>
2026-09-19 12:26:52 +0530

    feat: setup terraform modules and github action workflow

---
+++ environment/dev/main.tf
@@ -0,0 +1,6 @@
+module "azurerm_resource_group"{
+
+    source="../../modules/azurerm_resource_group"
+    rg=var.rg1
+
+}
---
+++ environment/prod/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_providers {
+      azurerm={
+        source ="hashicorp/azurerm"
+        version="4.81.0"
+      }
+    }
+
+    backend "azurerm"{
+      container_name = "finalaction"
+      resource_group_name = "rgsept"
+      storage_account_name = "resuableaction"
+      key = "tfstae_prod"
+    }
+}
+
+provider "azurerm"{
+    features{}
+}
+
+
